d3deab0b9b193cd53dfeec05ed0d22ee08baed84
Emily Vanaselja <emily.vanaselja@selu.edu>
2023-11-19 21:54:35 +0000

    Fixed filter_sexes and added each function to tutorial

--- R/filter_sexes.R
+++ R/filter_sexes.R
@@ -3,20 +3,22 @@
 #' Filters out any sexes, such as male, female, or others by inputting the data name, the variable that the sex is called, and which sex to filter out.
 #' 
 #' @param data Needs to be a data set that contains sexes in the form of strings.
-#' @param  s_vars The variable that the list of sexes is connect to. Must be placed in quotation marks.
+#' @param  s_vars The variable that the list of sexes is connect to. 
 #' @param s The sex that is to be filtered out. Must be placed in quotation marks.
 #' @return filt The complete data set without the sex that has been filtered out.
 #' 
 #' @export 
 
 filter_sexes <- function(data, s_vars, s) {
-    filt <- data %>% 
-      filter({{ s_vars }} != {{ s }})
-    if ((s_vars) != {{s}}) {
-          return(filt) 
-      } else {
-        print("Sex not filtered.")
-      }
+  filt <- data %>% 
+    filter({{s_vars}} != {{s}})
+  
+  if (nrow(filt) < nrow(data)) {
+    return(filt) 
+  } else {
+    print("Sex not filtered.")
+  }
 }
+
 
 
